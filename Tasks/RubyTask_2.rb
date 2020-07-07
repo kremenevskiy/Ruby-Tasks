c57455894fcc577aba86cdec3ecc40512ca07b01
Kremenevskiy <vkremenevskiy@gmail.com>
2020-07-07 17:02:25 +0300

    Added second approach to minitask 2

--- Tasks/RubyTask_2.rb
+++ Tasks/RubyTask_2.rb
@@ -6,12 +6,25 @@
 
 # Mini Task #2
 # Сделать возможным safe_invert для Хэша
-
+puts "\nTask #2\n"
 class Hash
   def save_invert
     inverted_hash = Hash.new
     self.each do |key, value|
       inverted_hash[value] = key
+    end
+    inverted_hash
+  end
+
+  # approach 2, using arrays
+  def save_invert_2
+    inverted_hash = Hash.new(Array.new)
+    self.each do |key, value|
+      if inverted_hash.has_key?(value)
+        inverted_hash[value] += [key]
+      else
+        inverted_hash[value] = [key]
+      end
     end
     inverted_hash
   end
@@ -24,11 +37,15 @@ fruits_country = {
 }
 
 inverted_hash = fruits_country.save_invert
-puts inverted_hash
+inverted_hash_2 = fruits_country.save_invert_2
+puts "Approach 1: #{inverted_hash}"
+puts "Approach 2: #{inverted_hash_2}"
+
 
 
 # Mini Task #3
 # На массиве реализовать метод получения Хэша (ключ - элемент, значение - количество повторений в массиве)
+puts "\nTask #3\n"
 
 class Array
   def to_histogram
